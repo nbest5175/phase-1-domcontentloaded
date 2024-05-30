c17903e5944ebeb6a22a7749f5bdc1a98145da33
Nicholas <bestnicholas05@gmail.com>
2024-05-30 19:31:49 -0400

    Completed DomContentLoaded Lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    
+    // Target the paragraph element with id="text" and replace its text content
+    const paragraph = document.getElementById("text");
+    paragraph.textContent = "This is really cool!";
+  });
+  
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
